c9a7f7966c249e1cb20910f2aa93de78c060586b
Kyeong Jin Jo <rafesia1337@kyonggi.ac.kr>
2018-05-03 15:01:24 +0900

    play에서 불러오고자하는 리스트 번호 입력 후 해당하는 번호의 데이터 불러오기

--- Project27/소스.c
+++ Project27/소스.c
@@ -1,20 +1,22 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include <conio.h>
+#include <mmsystem.h>
 #include <ctype.h>
+#pragma comment(lib, "winmm.lib")
 
 #define MAX_CHAR_PER_LINE 1000
 
 
 
-// 연결 리스트
+// 연결 리스트 데이터 지정
 typedef struct {
 	char data[MAX_CHAR_PER_LINE];
 } dataName; // 해당 파일이름, 길이지정
 
-//노드
+//노드 구성, data && link
 typedef struct Node {
 	dataName data;
 	struct Node *link;
@@ -76,7 +78,10 @@ void reverse_lines(Node **phead) {
 		q->link = r;
 	}
 	*phead = q;
+
+	printf("Reverse sort successful.\n\n");
 }
+
 
 int get_length(LinkedList *list) {
 	return list->length;
@@ -128,21 +133,77 @@ int is_empty(LinkedList *list) {
 	else return 0;
 }
 
+void insert_music(LinkedList *buffer) {
+	int pos;
+	char line[MAX_CHAR_PER_LINE];
+	dataName d;
+	
+	printf("Choose number to input:\n ");
+	scanf("%d", &pos);
+	pos -= 1;
+	printf("Input .wav File to add list\n ");
+	gets(stdin);
+	fflush(stdin);
+	fgets(line, MAX_CHAR_PER_LINE, stdin);
+	strcpy(d.data, line);
+	add(buffer, pos, d);
+	
+}
+
+void delete_music(LinkedList *buffer) {
+	int pos;
+
+	if (is_empty(buffer)) {
+		printf("Theres no musics.\n");
+	}
+	else {
+		printf("Delete music number: ");
+		scanf("%d", &pos);
+		delete(buffer, pos - 1);
+	}
+}
+
+void play(LinkedList *buffer) {
+	Node *tmp;
+	tmp = buffer->head;
+	int i;
+	int pos;
+	printf("Play music number: ");
+	scanf("%d", &pos);
+	int length = buffer->length;
+
+	if (pos == 1) {
+		printf("File name is %s", tmp->data.data);
+	}
+	else {
+		for (i = 0; i <= pos; i++) {
+			if (i == pos - 1) {
+				printf("File Name Is : %s", tmp->data.data);
+				break;
+			}
+			tmp = tmp->link;
+		}
+	}
+}
+
+
 
 void display(LinkedList *buffer) {
 	int i;
 	Node *tmp_node;
 	tmp_node = buffer->head;
 
-	printf("Player List\n");
+	printf("*****Player List*****\n");
 	for (i = 0; i < buffer->length; i++) {
 		printf("%s", tmp_node->data.data);
+		tmp_node = tmp_node->link;
 	}
+	printf("********************\n");
 }
 
 void showMenu() {
-	printf("Music Player Based on Console\n");
-	printf("Select Menu\n");
+	printf("\nMusic Player Based on Console\n\n");
+	printf("*********Select Menu********\n");
 	printf("0. Exit Music Player\n");
 	printf("1. Add Music\n");
 	printf("2. Delete Music from List\n");
@@ -152,11 +213,27 @@ void showMenu() {
 	printf("6. Show Current List\n");
 }
 
-void menu(int i) {
+
+
+void menu(int i, LinkedList *buffer) {
 	switch (i) {
 	case 1:
 		printf("You chosen number 1\n\n");
-
+		insert_music(buffer);
+		break;
+	case 2:
+		delete_music(buffer);
+		break;
+	case 3:
+		play(buffer);
+		break;
+	case 4:
+		break;
+	case 5:
+		reverse_lines(buffer);
+		break;
+	case 6:
+		display(buffer);
 	}
 }
 
@@ -170,7 +247,9 @@ int main() {
 	do {
 		showMenu();
 		scanf("%d", &input);
-		menu(input);
+		menu(input, &buffer);
 	} while (input != 0);
-	
+	if (input == 0) {
+		printf("Program Closed");
+	}
 }
